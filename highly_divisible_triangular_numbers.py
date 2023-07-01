1fe64f58df80d32a472d39e8eaf48345045b95c6
Luke Tutton <tuttonluke@gmail.com>
2023-07-01 14:15:48 +0100

    Adds efficient function.

--- highly_divisible_triangular_numbers.py
+++ highly_divisible_triangular_numbers.py
@@ -46,10 +46,88 @@ def first_triangular_with_n_divisors(n): # dumb algorithm
 def plot_divisors(nums, divisors):
     plt.scatter(nums, divisors)
 
+#%% Efficient algorithm
 
+def is_prime(n):
+    if n == 1:
+        return False
+    elif n < 4: # 2 & 3 are primes
+        return True
+    elif n % 2 == 0: # All primes > 2 are odd.
+        return False
+    elif n < 9: # 5 & 7 are primes
+        return True
+    elif n % 3 == 0:
+        return False
+    else:
+        r = np.floor(np.sqrt(n)) # rounded to largest integer such that r x r < n
+        f = 5 # already excluded multiples of 2 & 3
+        while f < r + 1:
+            if n % f == 0:
+                return False
+            elif n % (f+2) == 0: # no point checking f+1 because it will be even
+                return False
+            f += 6 # all primes > 3 can be written in the form 6k +/- 1
+        return True # in all other cases
 
+def nth_prime(n):
+    if n == 1:
+        return 2
+    else:
+        num = 1
+        count = 1 # we know that 2 is a prime
+        while count < n:
+            num += 2
+            if is_prime(num):
+                print(num)
+                count += 1
+        return num
+
+def prime_array(length_n):
+    prime_array = np.array(2)
+    if length_n == 1:
+        return prime_array
+    else:
+        num = 1
+        count = 1 # we know that 2 is a prime
+        while count < length_n:
+            num += 2
+            if is_prime(num):
+                prime_array = np.append(prime_array, num)
+                count += 1
+        return prime_array
+
+def first_triangular_with_n_divisors_fast(n):
+    num = 3 # start with a prime
+    Dn = 2 # number of divisors for any prime
+    count = 0 # ensure the while loop is entered
+    array = prime_array(169) # generate primes up to first above 1000
+
+    while count <= n:
+        n = n + 1
+        n1 = n
+        if n1 % 2 == 0:
+            n1 /= 2
+        Dn1 = 1
+
+        for i in range(1009):
+            if array[i]**2 > n1:
+                Dn1 *= 2
+                break
+
+            exponent = 1
+            while n1 % array[i] == 0:
+                exponent += 1
+                n1 /= array[i]
+            if exponent > 1:
+                Dn1 *= exponent
+            if n1 == 1:
+                break
+        count = Dn*Dn1
+        print(count)
+        Dn = Dn1
+    print((num * (num-1)) / 2)
 
 # %%
 if __name__ == "__main__":
-    nums, divisors = first_triangular_with_n_divisors(50)
-    plot_divisors(nums, divisors)
+    first_triangular_with_n_divisors_fast(500)
